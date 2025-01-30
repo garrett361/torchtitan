95f3dca3f49383af45c49c2f123d576d5846acd3
Garrett Goon <garrett361@gmail.com>
2025-01-30 19:45:59 +0000

    basic tp tests started

--- tests/bamba/test_dist.py
+++ tests/bamba/test_dist.py
@@ -2,6 +2,11 @@ import pytest
 from dtest import DTest
 import torch
 import torch.distributed as dist
+from copy import deepcopy
+from torch.distributed.device_mesh import init_device_mesh
+
+from torchtitan.models.llama.model import ModelArgs, Transformer
+from torchtitan.parallelisms.parallelize_llama import apply_tp as apply_tp_llama
 
 
 class TestDTest(DTest):
@@ -31,6 +36,35 @@ class TestDTest(DTest):
         self.print_rank0_only(f"{n=}")
 
 
-def test_regular():
-    print("In regular test")
-    assert True
+class TestTPTransformers(DTest):
+    requires_cuda_env = True
+    model_args = ModelArgs(n_layers=1, dim=256, n_heads=8, vocab_size=128)
+    batch_size = 1
+    seq_len = 32
+
+    def test_basic_tp(self) -> None:
+        torch.manual_seed(42)
+        mesh = init_device_mesh(
+            "cuda", mesh_shape=(self.get_world_size(),), mesh_dim_names=("tp",)
+        )
+        model = Transformer(self.model_args).to(
+            dtype=torch.bfloat16, device=self.get_device()
+        )
+        model_copy = deepcopy(model)
+        apply_tp_llama(
+            model,
+            mesh,
+            loss_parallel=False,
+            enable_float8=False,
+            enable_async_tp=False,
+        )
+        inputs = torch.randint(
+            self.model_args.vocab_size,
+            size=(self.batch_size, self.seq_len),
+            device=self.get_device(),
+        )
+        outputs_tp = model(inputs)
+        outputs = model_copy(inputs)
+
+        tol = 1e-2  # Fails on 1e-3
+        torch.testing.assert_close(outputs, outputs_tp, atol=tol, rtol=tol)
