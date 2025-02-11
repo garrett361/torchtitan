6adc17168ce5f0dc1b36098c3c101f3b5a9f373f
Garrett Goon <garrett361@gmail.com>
2025-02-11 16:31:17 +0000

    replace more {Column,Row}ParallelLinears

--- torchtitan/models/deepseek_v3/model.py
+++ torchtitan/models/deepseek_v3/model.py
@@ -228,19 +228,17 @@ class MLA(nn.Module):
         self.v_head_dim = args.v_head_dim
 
         if self.q_lora_rank == 0:
-            self.wq = ColumnParallelLinear(self.dim, self.n_heads * self.qk_head_dim)
+            self.wq = nn.Linear(self.dim, self.n_heads * self.qk_head_dim)
         else:
-            self.wq_a = Linear(self.dim, self.q_lora_rank)
+            self.wq_a = nn.Linear(self.dim, self.q_lora_rank)
             self.q_norm = build_norm(self.q_lora_rank)
-            self.wq_b = ColumnParallelLinear(
-                self.q_lora_rank, self.n_heads * self.qk_head_dim
-            )
+            self.wq_b = nn.Linear(self.q_lora_rank, self.n_heads * self.qk_head_dim)
         self.wkv_a = Linear(self.dim, self.kv_lora_rank + self.qk_rope_head_dim)
         self.kv_norm = build_norm(self.kv_lora_rank)
-        self.wkv_b = ColumnParallelLinear(
+        self.wkv_b = nn.Linear(
             self.kv_lora_rank, self.n_heads * (self.qk_nope_head_dim + self.v_head_dim)
         )
-        self.wo = RowParallelLinear(self.n_heads * self.v_head_dim, self.dim)
+        self.wo = nn.Linear(self.n_heads * self.v_head_dim, self.dim)
         self.softmax_scale = self.qk_head_dim**-0.5
         if args.max_seq_len > args.original_seq_len:
             mscale = 0.1 * args.mscale * math.log(args.rope_factor) + 1.0
@@ -630,7 +628,7 @@ class DeepSeekV3(nn.Module):
         for layer_id in range(args.n_layers):
             self.layers.append(Block(layer_id, args))
         self.norm = build_norm(args.dim)
-        self.head = ColumnParallelLinear(
+        self.head = nn.Linear(
             args.dim, args.vocab_size, dtype=torch.get_default_dtype()
         )
         self.register_buffer("freqs_cis", precompute_freqs_cis(args), persistent=False)
