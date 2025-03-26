77e948e693631a58108db4a73a2a9efd9e9b300a
Garrett Goon <garrett361@gmail.com>
2025-03-26 16:11:33 +0000

    fwd/bwd ep tests passing

--- tests/deepseek-v3/test_dist.py
+++ tests/deepseek-v3/test_dist.py
@@ -1,8 +1,11 @@
 from typing import Any
 
+import pytest
 import torch
 import torch.nn as nn
-from torch.distributed.device_mesh import init_device_mesh
+from torch.distributed.device_mesh import DeviceMesh, init_device_mesh
+from torch.distributed.fsdp import fully_shard
+from torch.distributed.tensor import DTensor, Replicate
 
 from dtest import DTest
 from torchtitan.models.deepseek_v3.model import (
@@ -15,6 +18,25 @@ def _copy_params(dst: nn.Module, src: nn.Module) -> None:
     with torch.no_grad():
         for p_dest, p_src in zip(dst.parameters(), src.parameters()):
             p_dest.data.copy_(p_src.data)
+
+
+def _test_grads(
+    mod: nn.Module, mod_fsdp: nn.Module, tol: float, mesh: DeviceMesh
+) -> None:
+    for (n, p), (_, p_fsdp) in zip(mod.named_parameters(), mod_fsdp.named_parameters()):
+        if p.grad is None:
+            assert p_fsdp.grad is None
+            return
+        grad = p.grad
+        grad_fsdp = p_fsdp.grad
+        if isinstance(grad_fsdp, DTensor):
+            grad_fsdp = grad_fsdp.redistribute(
+                mesh, placements=[Replicate() for _ in grad_fsdp.placements]
+            ).to_local()
+        try:
+            torch.testing.assert_close(grad, grad_fsdp, atol=tol, rtol=tol)
+        except Exception as e:
+            raise RuntimeError(f"Failed on {n=}") from e
 
 
 class TestEP(DTest):
@@ -67,7 +89,7 @@ class TestEP(DTest):
             v_head_dim=self.v_head_dim,
         )
 
-    def test_moe(self) -> None:
+    def test_moe_ep_fwd(self) -> None:
         torch.manual_seed(42)
         moe = MoE(self.model_args).to(**self.factory_kwargs)
         ep_mesh = init_device_mesh(
@@ -89,4 +111,54 @@ class TestEP(DTest):
         )
         outputs = moe(inputs)
         outputs_ep = moe_ep(inputs)
+
         torch.testing.assert_close(outputs, outputs_ep, atol=self.tol, rtol=self.tol)
+
+    @pytest.mark.gpu
+    def test_moe_ep_bwd(self) -> None:
+        torch.manual_seed(42)
+        moe = MoE(self.model_args).to(**self.factory_kwargs)
+        ep_mesh = init_device_mesh(
+            self.device_type, (self.world_size,), mesh_dim_names=("ep",)
+        )
+        moe_ep = MoE(self.model_args, ep_mesh).to(**self.factory_kwargs)
+        # Force models equal
+        _copy_params(moe_ep.gate, moe.gate)
+        if self.n_shared_experts > 0:
+            _copy_params(moe_ep.shared_experts, moe.shared_experts)
+        if self.n_routed_experts > 0:
+            for idx in moe_ep.experts:
+                _copy_params(moe_ep.experts[idx], moe.experts[idx])
+
+        fully_shard(moe_ep.gate, mesh=ep_mesh)
+        if moe_ep.shared_experts is not None:
+            fully_shard(moe_ep.shared_experts, mesh=ep_mesh)
+        fully_shard(moe_ep, mesh=ep_mesh, ignored_params=moe_ep.experts.parameters())
+
+        inputs = torch.randn(
+            self.world_size * self.batch_size,
+            self.seq_len,
+            self.dim,
+            **self.factory_kwargs,
+        )
+        outputs = moe(inputs)
+
+        inputs_ep = inputs.tensor_split(self.world_size, dim=0)[self.rank]
+        outputs_ep = moe_ep(inputs_ep)
+
+        # Grads should match with an aver-over-batches type loss
+        outputs.pow(2).mean().backward()
+        outputs_ep.pow(2).mean().backward()
+
+        _test_grads(moe.gate, moe_ep.gate, tol=self.tol, mesh=ep_mesh)
+        if moe.shared_experts is not None:
+            _test_grads(
+                moe.shared_experts, moe_ep.shared_experts, tol=self.tol, mesh=ep_mesh
+            )
+        for exp_idx in moe_ep.experts:
+            _test_grads(
+                moe.experts[exp_idx],
+                moe_ep.experts[exp_idx],
+                tol=self.tol,
+                mesh=ep_mesh,
+            )
