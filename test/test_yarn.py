f5261e16b41cd3a3e6fe1f486c684e3ac12e1837
Garrett Goon <goon@ibm.com>
2025-08-28 21:04:46 +0000

    more yarn tests

--- test/test_yarn.py
+++ test/test_yarn.py
@@ -9,6 +9,10 @@ from transformers.models.llama.modeling_llama import (
     LlamaRotaryEmbedding,
     apply_rotary_pos_emb,
 )
+from vllm.model_executor.layers.rotary_embedding import (
+    YaRNScalingRotaryEmbedding,
+    get_rope,
+)
 
 from torchtitan.models.llama.model import (
     apply_rotary_emb,
@@ -45,20 +49,14 @@ class TestYaRN:
     factor_default = 16.0
     num_attention_heads = 1  # Avoid OOMs
     device = "cuda"
-    dtype = torch.bfloat16
+    dtype = torch.float32
     factory_kwargs = {"device": device, "dtype": dtype}
     config_dir = pathlib.Path(__file__).parent / "configs"
+    tol=1e-4
 
     def setup_method(self, method):
         torch.manual_seed(42)
-        if transformers.__version__ != TRANSFORMERS_VERSION:
-            raise RuntimeError(
-                f"Expected {transformers.__version__}={TRANSFORMERS_VERSION}, got {transformers.__version__=}"
-            )
-        if vllm.__version__ != VLLM_VERSION:
-            raise RuntimeError(
-                f"Expected {vllm.__version__}={VLLM_VERSION}, got {vllm.__version__=}"
-            )
+        print(f"Running on {transformers.__version__=}, {vllm.__version__=}")
 
     def get_hf_cfg(self, seq_len: int) -> LlamaConfig:
         if seq_len <= ORIG_SEQ_LEN:
@@ -121,16 +119,16 @@ class TestYaRN:
         )
         # Check titan vs non-redundant hf entries
         torch.testing.assert_close(
-            titan_cos[None].to(hf_cos.dtype),
+            titan_cos[None],
             hf_cos[..., hf_cfg.head_dim // 2 :],
-            atol=1e-1,
-            rtol=1e-1,
+            atol=self.tol,
+            rtol=self.tol,
         )
         torch.testing.assert_close(
-            titan_sin[None].to(hf_cos.dtype),
+            titan_sin[None],
             hf_sin[..., hf_cfg.head_dim // 2 :],
-            atol=1e-1,
-            rtol=1e-1,
+            atol=self.tol,
+            rtol=self.tol,
         )
 
     @pytest.mark.parametrize(
@@ -184,37 +182,140 @@ class TestYaRN:
         ],
     )
     def test_hf_scores(self, seq_len: int) -> None:
-        hf_cfg = self.get_hf_cfg(seq_len=seq_len)
-        position_ids = torch.arange(seq_len, device=self.device)[None]
-        llama_rotary_emb = LlamaRotaryEmbedding(self.get_hf_cfg(seq_len=seq_len)).cuda()
-        q_hf, k_hf, q_titan, k_titan = self.get_q_ks(seq_len, hf_cfg)
+        with torch.inference_mode():
+            hf_cfg = self.get_hf_cfg(seq_len=seq_len)
+            position_ids = torch.arange(seq_len, device=self.device)[None]
+            llama_rotary_emb = LlamaRotaryEmbedding(self.get_hf_cfg(seq_len=seq_len)).cuda()
+            q_hf, k_hf, q_titan, k_titan = self.get_q_ks(seq_len, hf_cfg)
 
-        q_hf, k_hf = apply_rotary_pos_emb(
-            q_hf.transpose(1, 2),
-            k_hf.transpose(1, 2),
-            *llama_rotary_emb(x=k_hf, position_ids=position_ids),
+            q_hf, k_hf = apply_rotary_pos_emb(
+                q_hf.transpose(1, 2),
+                k_hf.transpose(1, 2),
+                *llama_rotary_emb(x=k_hf, position_ids=position_ids),
+            )
+            q_hf, k_hf = q_hf.transpose(1, 2), k_hf.transpose(1, 2)
+
+            titan_freqs_cis = precompute_freqs_cis(
+                dim=hf_cfg.head_dim,
+                seq_len=seq_len,
+                original_seq_len=ORIG_SEQ_LEN,
+                rope_theta=hf_cfg.rope_theta,
+                rope_factor=self.factor_default,
+                beta_fast=self.beta_fast_default,
+                beta_slow=self.beta_slow_default,
+            ).cuda()
+
+            q_titan, k_titan = apply_rotary_emb(q_titan, k_titan, titan_freqs_cis)
+            del titan_freqs_cis
+
+            # Check scores: hf_scores.shape = (bsz, n_heads, seq_len, seq_len)
+            hf_scores = q_hf.permute(0, 2, 1, 3) @ k_hf.permute(0, 2, 3, 1)
+            titan_scores = q_titan.permute(0, 2, 1, 3) @ k_titan.permute(0, 2, 3, 1)
+
+            # torch.testing.assert_close can OOM for long seqlen
+            abs_mean_diff = hf_scores - titan_scores
+            titan_scores_abs_mean = titan_scores.abs().mean()
+            del hf_scores, titan_scores
+            abs_mean_diff = abs_mean_diff.abs().mean() / titan_scores_abs_mean
+            assert abs_mean_diff < self.tol, f"{abs_mean_diff=}"
+
+    @pytest.mark.parametrize(
+        "seq_len",
+        [
+            2 * ORIG_SEQ_LEN,
+            4 * ORIG_SEQ_LEN,
+            8 * ORIG_SEQ_LEN,
+        ],
+    )
+    def test_vllm_cos_sin(self, seq_len: int) -> None:
+        with torch.inference_mode():
+            hf_cfg = self.get_hf_cfg(seq_len=seq_len)
+            vllm_rope = get_rope(
+                hf_cfg.head_dim,
+                rotary_dim=hf_cfg.head_dim,
+                max_position=hf_cfg.max_position_embeddings,
+                base=hf_cfg.rope_theta,
+                rope_scaling=hf_cfg.rope_scaling,
+                is_neox_style=False,
+                partial_rotary_factor=getattr(hf_cfg, "partial_rotary_factor", 1),
+            ).cuda()
+            assert isinstance(vllm_rope, YaRNScalingRotaryEmbedding)
+
+            q, k, q_titan, k_titan = self.get_q_ks(seq_len, hf_cfg)
+            vllm_cos, vllm_sin = vllm_rope.cos_sin_cache.chunk(2, dim=-1)
+            titan_freqs_cis = precompute_freqs_cis(
+                dim=hf_cfg.head_dim,
+                seq_len=seq_len,
+                original_seq_len=ORIG_SEQ_LEN,
+                rope_theta=hf_cfg.rope_theta,
+                rope_factor=self.factor_default,
+                beta_fast=self.beta_fast_default,
+                beta_slow=self.beta_slow_default,
+            ).cuda()
+
+            titan_cos_sin = torch.view_as_real(titan_freqs_cis)
+            titan_cos, titan_sin = titan_cos_sin[..., 0], titan_cos_sin[..., 1]
+            # The vllm cos/sin have the full seqlen, so compare slices.
+            torch.testing.assert_close(
+                titan_cos,
+                vllm_cos[: titan_cos.shape[0]],
+                atol=self.tol,
+                rtol=self.tol,
+            )
+            torch.testing.assert_close(
+                titan_sin,
+                vllm_sin[: titan_cos.shape[0]],
+                atol=self.tol,
+                rtol=self.tol,
+            )
+
+        @pytest.mark.parametrize(
+            "seq_len",
+            [
+                2 * ORIG_SEQ_LEN,
+                4 * ORIG_SEQ_LEN,
+                8 * ORIG_SEQ_LEN,
+            ],
         )
-        q_hf, k_hf = q_hf.transpose(1, 2), k_hf.transpose(1, 2)
+        def test_vllm_scores(self, seq_len: int) -> None:
+            hf_cfg = self.get_hf_cfg(seq_len=seq_len)
+            vllm_rope = get_rope(
+                hf_cfg.head_dim,
+                rotary_dim=hf_cfg.head_dim,
+                max_position=hf_cfg.max_position_embeddings,
+                base=hf_cfg.rope_theta,
+                rope_scaling=hf_cfg.rope_scaling,
+                is_neox_style=False,
+                partial_rotary_factor=getattr(hf_cfg, "partial_rotary_factor", 1),
+            ).cuda()
+            assert isinstance(vllm_rope, YaRNScalingRotaryEmbedding)
 
-        titan_freqs_cis = precompute_freqs_cis(
-            dim=hf_cfg.head_dim,
-            seq_len=seq_len,
-            original_seq_len=ORIG_SEQ_LEN,
-            rope_theta=hf_cfg.rope_theta,
-            rope_factor=self.factor_default,
-            beta_fast=self.beta_fast_default,
-            beta_slow=self.beta_slow_default,
-        ).cuda()
+            position_ids = torch.arange(seq_len, device=self.device)[None]
 
-        q_titan, k_titan = apply_rotary_emb(q_titan, k_titan, titan_freqs_cis)
-        del titan_freqs_cis
+            _, _, q, k = self.get_q_ks(seq_len, hf_cfg)
 
-        # Check scores: hf_scores.shape = (bsz, n_heads, seq_len, seq_len)
-        hf_scores = q_hf.permute(0, 2, 1, 3) @ k_hf.permute(0, 2, 3, 1)
-        titan_scores = q_titan.permute(0, 2, 1, 3) @ k_titan.permute(0, 2, 3, 1)
+            titan_freqs_cis = precompute_freqs_cis(
+                dim=hf_cfg.head_dim,
+                seq_len=seq_len,
+                original_seq_len=ORIG_SEQ_LEN,
+                rope_theta=hf_cfg.rope_theta,
+                rope_factor=self.factor_default,
+                beta_fast=self.beta_fast_default,
+                beta_slow=self.beta_slow_default,
+            ).cuda()
 
-        # torch.testing.assert_close is hard to pass with so many elements:
-        mean_abs_diff = (
-            hf_scores - titan_scores
-        ).abs().mean() / titan_scores.abs().mean()
-        assert mean_abs_diff < 1e-2, f"{mean_abs_diff=}"
+            q_titan, k_titan = apply_rotary_emb(q, k, titan_freqs_cis)
+            q_vllm, k_vllm = vllm_rope(position_ids, q, k)
+            del q, k, titan_freqs_cis, position_ids
+
+            # Check scores: vllm_scores.shape = (bsz, n_heads, seq_len, seq_len)
+            vllm_scores = q_vllm.permute(0, 2, 1, 3) @ k_vllm.permute(0, 2, 3, 1)
+            titan_scores = q_titan.permute(0, 2, 1, 3) @ k_titan.permute(0, 2, 3, 1)
+            del q_vllm, k_vllm, q_titan, k_titan
+
+            # torch.testing.assert_close can OOM for long seqlen
+            abs_mean_diff = vllm_scores - titan_scores
+            titan_scores_abs_mean = titan_scores.abs().mean()
+            del vllm_scores, titan_scores
+            abs_mean_diff = abs_mean_diff.abs().mean() / titan_scores_abs_mean
+            assert abs_mean_diff < self.tol, f"{abs_mean_diff=}"
