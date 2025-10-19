e76a73e5636c5db9f13c617ee93ed220213408c7
Garrett Goon <goon@ibm.com>
2025-10-19 05:17:47 +0000

    refactor custom args into override classes

--- torchtitan/models/llama3_moe/custom_args.py
+++ torchtitan/models/llama3_moe/custom_args.py
@@ -5,6 +5,7 @@
 # LICENSE file in the root directory of this source tree.
 
 from dataclasses import dataclass, field
+from typing import Literal
 
 from torchtitan.config.job_config import JobConfig
 
@@ -14,10 +15,7 @@ class Llama3MoECustomArgs:
     """
     Catch-all, misc. cfg.
     """
-
-    load_balance_coeff: float | None = None
     hf_weight_transform: str = "replicate"
-    hf_ffn_hidden_dim: int | None = None
 
 
 @dataclass
@@ -28,7 +26,37 @@ class TopKSchedulerArgs:
     warmup_steps: int | None = None
 
 
+# Classes for overriding model architecture configurations, which can't be done via the CLI or toml
+# files otherwise. When non-None values are supplied here, a value will be overridden. Some
+# convenience fields are also added:
+# - n_moe_layers: int, add this many moe layers starting from the final layer.
+
+
+@dataclass
+class ModelOverrides:
+    moe_inter_dim: int | None = None
+    n_layers: int | None = None
+    custom_moe_impl: str | None = None
+    n_moe_layers: int | None = None
+
+
+@dataclass
+class MoEOverrides:
+    num_experts: int | None = None
+    num_shared_experts: int | None = None
+    score_func: Literal["softmax", "sigmoid"] | None = None
+    route_norm: bool | None = None
+    route_scale: float | None = None
+    score_before_experts: bool | None = None
+    top_k: int | None = None
+    use_grouped_mm: bool | None = None
+    load_balance_coeff: float | None | None = None
+    hf_ffn_hidden_dim: int | None | None = None
+
+
 @dataclass
 class Llama3MoEJobConfig(JobConfig):
     custom_args: Llama3MoECustomArgs = field(default_factory=Llama3MoECustomArgs)
     top_k_args: TopKSchedulerArgs = field(default_factory=TopKSchedulerArgs)
+    model_overrides: ModelOverrides = field(default_factory=ModelOverrides)
+    moe_overrides: MoEOverrides = field(default_factory=MoEOverrides)
