429d9a3576d11178a2e2e16301aaab0ee027b5a2
Garrett Goon <goon@ibm.com>
2025-11-13 20:42:02 +0000

    rm min/max tok metrics

--- torchtitan/models/llama3_moe/metrics.py
+++ torchtitan/models/llama3_moe/metrics.py
@@ -30,16 +30,16 @@ class CustomMetricsProcessor(MetricsProcessor):
             for block_idx, transformer_block in model_part.layers.items():
                 if not transformer_block.moe_enabled:
                     continue
-                moe_metrics[
-                    f"moe_entropy/layer_{block_idx}"
-                ] = self.get_normalized_entropy(transformer_block)
+                moe_metrics[f"moe_entropy/layer_{block_idx}"] = (
+                    self.get_normalized_entropy(transformer_block)
+                )
                 if (
                     n_expert_groups := model_part.model_args.moe_args.n_expert_groups
                 ) > 1:
-                    moe_metrics[
-                        f"moe_group_entropy/layer_{block_idx}"
-                    ] = self.get_expert_group_normalized_group_entropy(
-                        transformer_block, n_expert_groups
+                    moe_metrics[f"moe_group_entropy/layer_{block_idx}"] = (
+                        self.get_expert_group_normalized_group_entropy(
+                            transformer_block, n_expert_groups
+                        )
                     )
                 # Reset
                 transformer_block.moe.tokens_per_expert_cumulative.zero_()
@@ -49,15 +49,9 @@ class CustomMetricsProcessor(MetricsProcessor):
                 moe_metrics[f"moe_router/layer_{block_idx} abs mean"] = (
                     router_weight.abs().mean().item()
                 )
-                moe_metrics[
-                    f"moe_router/layer_{block_idx} std"
-                ] = router_weight.std().item()
-                moe_metrics[
-                    f"moe_router/layer_{block_idx} min"
-                ] = router_weight.min().item()
-                moe_metrics[
-                    f"moe_router/layer_{block_idx} max"
-                ] = router_weight.max().item()
+                moe_metrics[f"moe_router/layer_{block_idx} std"] = (
+                    router_weight.std().item()
+                )
 
         return moe_metrics
 
