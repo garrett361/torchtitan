c41dbe9108068fc83d14f98a7c9a1877011314a9
Garrett Goon <goon@ibm.com>
2025-11-14 13:22:10 +0000

    print spacing

--- torchtitan/components/metrics.py
+++ torchtitan/components/metrics.py
@@ -444,7 +444,7 @@ class MetricsProcessor:
 
         logger.info(
             f"{color.red}step: {step:2}  "
-            f"{color.yellow}time remaining: {time_remaining}"
+            f"{color.yellow}time remaining: {time_remaining}  "
             f"{color.green}loss: {global_avg_loss:7.4f}  "
             f"{color.orange}grad_norm: {grad_norm:7.4f}  "
             f"{color.turquoise}memory: {device_mem_stats.max_reserved_gib:5.2f}GiB"
