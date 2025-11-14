8098ed6d23897d375d1635d5eeb5e02d14171366
Garrett Goon <goon@ibm.com>
2025-11-14 17:21:06 +0000

    remove unused class

--- moe_bench_and_test/test_moe.py
+++ moe_bench_and_test/test_moe.py
@@ -5,54 +5,11 @@
 # LICENSE file in the root directory of this source tree.
 
 
-from typing import Any, Union
-
 import torch
 from einops import rearrange
 from triton.testing import do_bench
 
 from torchtitan.models.moe.moe import FeedForward, MoE, MoEArgs, MoEClone, MoEEmptyLike
-
-
-class CUDAMemContext:
-    def __init__(
-        self,
-        use_GiB: bool = True,
-        filter_patterns: tuple[str, ...] = ("allocated_bytes.all",),
-        mem_only: bool = True,
-    ) -> None:
-        self.use_GiB = use_GiB
-        self.filter_patterns = filter_patterns
-        self.mem_only = mem_only
-        self.before: dict[str, Union[int, float]] = {}
-        self.after: dict[str, Union[int, float]] = {}
-        self.delta: dict[str, Union[int, float]] = {}
-
-    def _get_mem_dict(self) -> dict[str, Union[int, float]]:
-        mem_dict = torch.cuda.memory_stats()
-        if self.filter_patterns:
-            mem_dict = {
-                k: v
-                for k, v in mem_dict.items()
-                if any(p in k for p in self.filter_patterns)
-            }
-        if self.mem_only:
-            mem_dict = {k: v for k, v in mem_dict.items() if "bytes" in k}
-        if self.use_GiB:
-            mem_dict = {
-                k.replace("bytes", "GiB"): v / 2**30 if "bytes" in k else v
-                for k, v in mem_dict.items()
-            }
-        return mem_dict
-
-    def __enter__(self) -> "CUDAMemContext":
-        self.before = self._get_mem_dict()
-        return self
-
-    def __exit__(self, *args: Any, **kwargs: Any) -> None:
-        torch.cuda.synchronize()
-        self.after = self._get_mem_dict()
-        self.delta = {k: v - self.before[k] for k, v in self.after.items()}
 
 
 # NOTE: @goon -  torch.testing.assert_close is very strict and hard to pass. Use the more-lenient
