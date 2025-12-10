7a263dbf1256327df9e2483d2da89ee21549bfd6
Garrett Goon <goon@ibm.com>
2025-12-10 22:00:43 +0000

    lint

--- torchtitan/models/llama3_moe/model/args.py
+++ torchtitan/models/llama3_moe/model/args.py
@@ -95,7 +95,7 @@ class Llama3MoEModelArgs(BaseModelArgs):
             )
 
         if (
-            getattr(self, "is_moe_list") is not None
+            getattr(self, "is_moe_list", None) is not None
             and len(self.is_moe_list) != self.n_layers
         ):
             raise ValueError(
