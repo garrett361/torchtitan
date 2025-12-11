ebd6629763321ab327310d04d2b3aa56fde708e3
Garrett Goon <goon@ibm.com>
2025-12-11 20:49:31 +0000

    try to llama3 apply_fsdp

--- torchtitan/models/llama3_moe/infra/parallelize.py
+++ torchtitan/models/llama3_moe/infra/parallelize.py
@@ -147,23 +147,35 @@ def parallelize_llama_moe(
                 dp_mod_ep_mesh_dim_names.append("dp_replicate")
             dp_mod_ep_mesh_dim_names.append("dp_shard_mod_ep")
 
+        # DELETE
+        from torchtitan.models.llama3.infra.parallelize import apply_fsdp
+
         apply_fsdp(
             model,
-            dp_mesh,
+            world_mesh[tuple(dp_mesh_dim_names)],
             param_dtype=TORCH_DTYPE_MAP[job_config.training.mixed_precision_param],
             reduce_dtype=TORCH_DTYPE_MAP[job_config.training.mixed_precision_reduce],
             pp_enabled=parallel_dims.pp_enabled,
             cpu_offload=job_config.training.enable_cpu_offload,
             reshard_after_forward_policy=job_config.parallelism.fsdp_reshard_after_forward,
-            moe_reshard_after_forward=job_config.moe_overrides.moe_reshard_after_forward,
-            ep_degree=parallel_dims.ep,
-            dp_mod_ep_mesh=(
-                world_mesh[tuple(dp_mod_ep_mesh_dim_names)]
-                if parallel_dims.ep_enabled
-                else None
-            ),
-            gradient_divide_factor=parallel_dims.fsdp_gradient_divide_factor,
         )
+        # apply_fsdp(
+        #     model,
+        #     dp_mesh,
+        #     param_dtype=TORCH_DTYPE_MAP[job_config.training.mixed_precision_param],
+        #     reduce_dtype=TORCH_DTYPE_MAP[job_config.training.mixed_precision_reduce],
+        #     pp_enabled=parallel_dims.pp_enabled,
+        #     cpu_offload=job_config.training.enable_cpu_offload,
+        #     reshard_after_forward_policy=job_config.parallelism.fsdp_reshard_after_forward,
+        #     moe_reshard_after_forward=job_config.moe_overrides.moe_reshard_after_forward,
+        #     ep_degree=parallel_dims.ep,
+        #     dp_mod_ep_mesh=(
+        #         world_mesh[tuple(dp_mod_ep_mesh_dim_names)]
+        #         if parallel_dims.ep_enabled
+        #         else None
+        #     ),
+        #     gradient_divide_factor=parallel_dims.fsdp_gradient_divide_factor,
+        # )
 
         if parallel_dims.dp_replicate_enabled:
             logger.info("Applied HSDP to the model")
